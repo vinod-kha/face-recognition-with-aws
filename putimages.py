fa799bd9f68680ee2c6170a8cbbbe043b95e6189
Vinod Khathi <87168769+vinod-kha@users.noreply.github.com>
2024-04-16 12:39:36 +0530

    Update putimages.py

--- putimages.py
+++ putimages.py
@@ -3,12 +3,11 @@ import boto3
 s3 = boto3.resource('s3')
 
 # Get list of objects for indexing
-images=[('image1.jpg','Elon Musk'),
-      ('image2.jpg','Elon Musk'),
-      ('image3.jpg','Bill Gates'),
-      ('image4.jpg','Bill Gates'),
-      ('image5.jpg','Sundar Pichai'),
-      ('image6.jpg','Sundar Pichai')
+images=[('Elon_Musk.jpg','Elon Musk'),
+      ('Elon_Muskvin.jpg','Elon Musk'),
+      ('Bill_Gates.jpg','Bill Gates'),
+      ('Sundar_Pichai.jpg','Sundar Pichai'),
+      ('Sunder_Pichaibhai.jpg','Sundar Pichai')
       ]
 
 # Iterate through list to upload objects to S3   
